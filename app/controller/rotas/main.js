ed30425e7fed35183b1e291cabe36fcd52688b29
Gislainy Crisostomo Velasco <gislainycrisostomo@gmail.com>
2017-05-19 14:44:08 -0300

    theme

--- app/controller/rotas/main.js
+++ app/controller/rotas/main.js
@@ -1,13 +1,13 @@
-Router.onBeforeAction(function () {
-  if (!Meteor.userId()) {
-    var correntRouter = Router.current().route.getName()
-    if(correntRouter === 'criarconta')
-      this.render('criarContaUsuario')
-    else this.render('login');
-  } else {
-    this.next();
-  }
-});;
+// Router.onBeforeAction(function () {
+//   if (!Meteor.userId()) {
+//     var correntRouter = Router.current().route.getName()
+//     if(correntRouter === 'criarconta')
+//       this.render('criarContaUsuario')
+//     else this.render('login');
+//   } else {
+//     this.next();
+//   }
+// });;
 
 Router.route('/', {
   template: 'main'
